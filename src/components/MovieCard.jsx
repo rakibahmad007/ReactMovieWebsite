9497f5fdc9977ed754b3870f3142683f381fdb68
rakibahmad007 <118014776+rakibahmad007@users.noreply.github.com>
2025-02-04 02:29:57 +0600

    Update MovieCard.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
-
-const MovieCard = () => {
+import  starImg from '../images/star.svg'
+const MovieCard = ({movie : {title, vote_average, poster_path, release_date, original_language}}) => {
+     {/* //we might have to write movie.id, movie.title and etc, instead we can destructure more properties from the movie object */}
   return (
-    <div>MovieCard</div>
+    <div className="movie-card">
+        <img 
+        src={
+            poster_path? `https://image.tmdb.org/t/p/w500/${poster_path}`: '/images/no-movie.png'
+        }  alt={title}/>
+
+        <div className="mt-4">
+            <h3>{title}</h3>
+            <div className="content">
+                <div className="rating">
+                    <img src={starImg} alt="" />
+                    <p>{vote_average? vote_average.toFixed(1): 'N/A'}</p>
+                </div>
+
+                <span>•</span>
+                <p className="lang">{original_language}</p>
+
+                <span>•</span>
+                <p className="year">{release_date ? release_date.split('-')[0]: 'N/A' }</p>
+            </div>
+        </div>
+       
+    </div>
   )
 }
 
